b3f973f054ae652959af22a2f876c03790cb8007
BeeUser12 <rosalina129@outlook.com>
2022-08-10 12:30:06 +0800

    Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace Exps
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.watermarkTextBox1 = new Exps.WatermarkTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 25);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "生成";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(103, 43);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            127,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(62, 23);
+            this.numericUpDown1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(255, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "拿出去让大家见识该字符的威力吧（doge）";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(171, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "注：仅̵̢̙͖̯̲̳̜͎ͫ̽̄̀ͯ̔̀供娱̵̵̢̢̙͖̯̙͖̯̲̳̜͎̲̳̜͎ͫ̽̄̀ͯ̔ͫ̽̄̀ͯ̔̀̀乐̵̢̙͖̯̲̳̜͎ͫ̽̄̀ͯ̔̀（doge）";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(140, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "请不要一直̧̖̝̼̝͖͓̲̗̹̖̪̞ͭ̾͊ͨͯ̌͒͗̈̀͒ͩͯ̅̃ͨ̀̇ͨ̃͜͡͝͏̴̸̵̢̰̤̼̱̌͆ͩ̓ͬ̏̉̄͜生̖̝̼̝ͭ̾͊ͨͯ̌͒͗̈̀͒ͩͯ̅͜͡͝" +
+    "̧͖͓̲̗̹̖̪̞̃ͨ̀̇ͨ̃͏̴̸̵̢̰̤̼̱̌͆ͩ̓ͬ̏̉̄͜成̧̖̝̼̝͖͓̲̗̹̖̪̞ͭ̾͊ͨͯ̌͒͗̈̀͒ͩͯ̅̃ͨ̀̇ͨ̃͜͡͝͏̴̸̰̤̌͆ͩ̓͜" +
+    "̵̢̼̱ͬ̏̉̄哦~";
+            // 
+            // watermarkTextBox1
+            // 
+            this.watermarkTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.watermarkTextBox1.Name = "watermarkTextBox1";
+            this.watermarkTextBox1.Size = new System.Drawing.Size(296, 23);
+            this.watermarkTextBox1.TabIndex = 4;
+            this.watermarkTextBox1.WatermarkText = "输入任意字符，也可以直接生成。";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(321, 125);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.watermarkTextBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "组合变音标记文字生成器";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private WatermarkTextBox watermarkTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Exps;
+
+namespace Exps
+{
+    public partial class Form1 : Form
+    {
+        int genTime; 
+        Random random = new Random((int)DateTime.Now.Ticks);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private string combinegens(string labelchar)
+        {
+            StringBuilder str = new StringBuilder();
+            char c = ' ';
+            for (int i = 0; i < genTime; i++)
+            {
+                c = Convert.ToChar(Convert.ToInt32(Math.Floor(112 * random.NextDouble() + 0x300)));
+                str.Append(c);
+            }
+            string actionchar = labelchar + str;
+            return Convert.ToString(actionchar);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            genTime = Convert.ToInt32(numericUpDown1.Value);
+            watermarkTextBox1.Text = combinegens(watermarkTextBox1.Text);
+        }
+    }
+}
